38a70aa884ea2e4a28a7c30fdc1e214ca1be5ab7
IlyaNazaruk <nazaruchok_1995@mail.ru>
2021-01-25 21:05:59 +0300

    update 1 task2

--- task_2/atm.php
+++ task_2/atm.php
@@ -11,7 +11,7 @@ class CashMachine {
     /**
      * Available bill values in the ATM. Consider quantities are unlimited.
      */
-    private $bills = array(2, 5, 10, 20, 50, 100);
+    private $bills = array(5, 10, 20, 50, 100, 200, 500);
     private $money_left;
     private $cash = array();
 
--- task_2/atm2.php
+++ task_2/atm2.php
@@ -1,16 +1,36 @@
 <?php
 
 $value = 0;
-$numl = 0;
-if (isset($_POST['btn'])){
-    $value = $_POST['value'];
-    if ($value == 0 || ""){
-        echo "value nor acceptable";
-    }
-    else{
-        $numl = $value / 5000;
-        $numls = (int)$numl;
-        echo "5000 notes :" . $numls . "br";
-    }
+$num1 = 0;
+$value = $_POST['value'];
+if ($value == 0 || ""){
+    echo "value not acceptable";
 }
+else {
+    $num1 = $value / 500;
+    $num1s = (int)$num1;
+    echo "500 notes :" . $num1s . "<br>";
+    $value = $value - ($num1s * 500);
+    $num2 = $value / 200;
+    $num2s = (int)$num2;
+    echo "200 notes :" . $num2s . "<br>";
+    $value = $value - ($num2s * 200);
+    $num3 = $value / 100;
+    $num3s = (int)$num3;
+    echo "100 notes :" . $num3s . "<br>";
+    $value = $value - ($num3s * 100);
+    $num4 = $value / 50;
+    $num4s = (int)$num4;
+    echo "50 notes :" . $num4s . "<br>";
+    $value = $value - ($num4s * 50);
+    $num5 = $value / 20;
+    $num5s = (int)$num5;
+    echo "50 notes :" . $num5s . "<br>";
+    $value = $value - ($num4s * 50);
+    $num5 = $value / 20;
+    $num5s = (int)$num5;
+    echo "50 notes :" . $num5s . "<br>";
+
+}
+
 
